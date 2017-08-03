de3a4adaaf7babc497008835e972adfa0b6697ce
IPGAP\frank.wang <frank.wang@mullenloweprofero.com>
2017-08-03 16:55:37 +0800

    remove extends rules airbnb

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,19 +3,33 @@
 module.exports = {
 
   parser: 'babel-eslint',
+  plugins : [
+    'react'
+  ],
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: "module",
+    ecmaFeatures: {
+      'jsx': true
+    }
+  },
 
   env: {
     browser: true,
     jest: true,
+    es6: true,
+    amd: true,
+    node: true,
+    mocha: true
   },
 
   globals: {
     "$": true
   },
 
-  extends: [
-    'airbnb'
-  ],
+  // extends: [
+  //   'airbnb'
+  // ],
 
   // add your custom rules here
   rules: {
@@ -32,6 +46,18 @@ module.exports = {
     'react/forbid-prop-types': 0,
 
     // allow dev module import in config files
-    'import/no-extraneous-dependencies': 0
+    'import/no-extraneous-dependencies': 0,
+    "comma-dangle": 1,
+    "quotes": [ 1, "single" ],
+    "no-undef": 1,
+    "global-strict": 0,
+    "no-extra-semi": 1,
+    "no-console": 1,
+    "no-unused-vars": 1,
+    "no-trailing-spaces": [1, { "skipBlankLines": true }],
+    "no-unreachable": 1,
+    "no-alert": 0,
+    "react/jsx-uses-react": 1,
+    "react/jsx-uses-vars": 1
   }
 }
