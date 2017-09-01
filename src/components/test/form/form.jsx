070f3bc469df3cc2fb13d6d30af0a8b2d3e20936
IPGAP\frank.wang <frank.wang@mullenloweprofero.com>
2017-09-01 15:28:13 +0800

    text file upload !

--- src/components/test/form/form.jsx
+++ src/components/test/form/form.jsx
@@ -1,9 +1,35 @@
 import React from 'react';
 import Style from './form.scss';
 import Validation from 'jquery-validation';
+import {Img} from './images/4.jpg'
 
 const From = React.createClass({
+    getInitiastate(){
+        return {
+            uploadedFile : '',
+            uploadedFileGetUrl : ''
+        }
+    },
+
+    error() {
+        alert('error');
+    },
+
+    callback(result) {
+        this.setState({
+            uploadedFile : result.uploadedFile,
+            uploadedFileGetUrl : result.uploadedFileGetUrl
+        })
+    },
+
     render(){
+
+        if (!this.state){
+            this.state = {
+                uploadedFile : '',
+                uploadedFileGetUrl : ''
+            }
+        }
         return(
             <div className="container">
                 <form method="get" action>
@@ -25,6 +51,20 @@ const From = React.createClass({
                     <input type="submit" className={Style['submit']} value='submit'/>
                 </form>
 
+                <h2>图片上传</h2>
+                <div className={Style['file-container']}>
+                    <input type="file" multiple accept='image/*' onChange={this.handleFiles}></input>
+                    <div className={Style['__file-list']}>
+                        {
+                            this.state.uploadedFileGetUrl === '' ? null :
+                            <div>
+                                <p>{this.state.uploadedFile}</p>
+                                <img src={this.state.uploadedFileGetUrl} alt='your choose image'></img>
+                            </div>
+                        }
+                    </div>
+                </div>
+
             </div>
         )
     },
@@ -42,25 +82,84 @@ const From = React.createClass({
                 confirm_password : {
                     required : true,
                     minlength : 5,
-                    equalTo : "#password"
+                    equalTo : '#password'
                 }
             },
             messages : {
                 email : 'Please enter right Email',
                 password : {
-                    required : "Enter Right Mail",
-                    minlength: "Your Must be at least 8 chaercters long",
+                    required : 'Enter Right Mail',
+                    minlength: 'Your Must be at least 8 chaercters long'
                 },
                 confirm_password : {
-                    required : "Enter Right Mail",
-                    minlength: "Your Must be at least 8 chaercters long",
-                    equalTo:"plase enter same password"
+                    required : 'Enter Right Mail',
+                    minlength: 'Your Must be at least 8 chaercters long',
+                    equalTo:'plase enter same password'
                 }
             }
         });
+    },
 
-        console.log(Validation)
+    handleFiles(e){
+        const file = e.target;
+        Http.post('./images/4.jpg', file, this.callback, this.error)
     }
 });
 
 export default From;
+
+var Http = (function() {
+    var http = {};
+    if (typeof window.XMLHttpRequest === "undefined") {
+        window.XMLHttpRequest = function() {
+            // 如果是i5就用Microsoft，其他就用Msxml2
+            return new window.ActiveXObject(navigator.userAgent
+                    .indexOf("MSIE 5") >= 0 ? "Microsoft.XMLHTTP"
+                    : "Msxml2.XMLHTTP");
+        };
+    }
+    http.post = function(url, data,  callback, error) {
+        if (typeof data === "function") {//data可以不穿值
+            callback = data;
+            data = null;
+        }
+        var timeout = setTimeout(function() {//超时设置
+            error();
+        }, 10000);
+        var xhr = new XMLHttpRequest();
+        xhr.open('post', url);
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState === 4) {
+                clearTimeout(timeout);//清除超时
+                if (xhr.status === 200){
+                    //alert(xhr.responseText);
+                    callback(JSON.parse(xhr.responseText));//调用回调函数
+                } else {
+                    error();
+                }
+                xhr = null;// 删除对象,防止内存溢出
+            }
+        };
+        xhr.onerror = function() {//如果产生了错误
+            clearTimeout(timeout);
+            error();
+        };
+        xhr.send(http.formDataCode(data));
+    };
+    http.formDataCode = function(data) {
+        var fd = new FormData();
+        if (!data) {
+            return null;
+        }
+        for ( var key in data) {
+            if(data.files){
+                var file=data.files[0];
+                fd.append("image", file);
+            }else{
+                fd.append(key, data[key]);
+            }
+        }
+        return fd;
+    }
+    return http;
+})();
